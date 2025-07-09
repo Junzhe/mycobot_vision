e60ce101fe5664c62b07099ed246c694b8b3a5df
Junzhe <163087301+Junzhe@users.noreply.github.com>
2025-07-09 16:11:51 +0800

    Create test_move.py

---
+++ scripts/test_move.py
@@ -0,0 +1,10 @@
+from pymycobot import MyCobot280
+from pymycobot import PI_PORT, PI_BAUD
+import time
+
+mc = MyCobot280(PI_PORT, PI_BAUD)
+
+print("正在尝试发送移动指令...")
+mc.send_coords([0, 0, 150, 180, 0, 0], 25, 0)
+time.sleep(3)
+print("已发送移动指令")
