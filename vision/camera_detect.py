b36df1a4b70caa3cb5d4ab03d35d01be2ae0bd1c
Junzhe <163087301+Junzhe@users.noreply.github.com>
2025-07-10 23:09:44 +0800

    Update camera_detect.py

--- vision/camera_detect.py
+++ vision/camera_detect.py
@@ -4,9 +4,9 @@ import stag
 import numpy as np
 import json
 import time
-from marker_utils import *
+from vision import marker_utils
 from scipy.linalg import svd
-from pymycobot import *
+from pymycobot.mycobot import MyCobot
 mc = MyCobot280("/dev/ttyAMA0", 115200)  # 需要手动设置端口及型号
 # mc = MyCobot320("COM32")  # 需要手动设置端口及型号
 type = mc.get_system_version()
